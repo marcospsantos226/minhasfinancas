e4209900f05e7664e6590e8829f5dd363cbbddde
marcospsantos226 <marcospsantos226@gmail.com>
2023-01-28 14:20:55 -0300

    Implementacao do metodo salvar - LancamentoServiceImpl

--- src/main/java/com/marcosp/minhasfinancas/service/impl/LancamentoServiceImpl.java
+++ src/main/java/com/marcosp/minhasfinancas/service/impl/LancamentoServiceImpl.java
@@ -2,14 +2,29 @@ package com.marcosp.minhasfinancas.service.impl;
 
 import com.marcosp.minhasfinancas.model.entity.Lancamento;
 import com.marcosp.minhasfinancas.model.enums.StatusLancamento;
+import com.marcosp.minhasfinancas.model.repository.LancamentoRepository;
 import com.marcosp.minhasfinancas.service.LancamentoService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 
 import java.util.List;
 
+@Service
 public class LancamentoServiceImpl implements LancamentoService {
+
+    private LancamentoRepository repository;
+
+    public LancamentoServiceImpl(LancamentoRepository repository){
+        this.repository = repository;
+    }
+
     @Override
+    @Transactional
     public Lancamento salvar(Lancamento lancamento) {
-        return null;
+
+
+        return repository.save(lancamento);
     }
 
     @Override
