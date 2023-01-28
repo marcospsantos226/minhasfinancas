7bf55033c29221582957dd7b76c75ec89d77c876
marcospsantos226 <marcospsantos226@gmail.com>
2023-01-28 14:03:10 -0300

    Implementacao Interface LancamentoService + Criacao da Class LancamentoServiceImpl

---
+++ src/main/java/com/marcosp/minhasfinancas/service/impl/LancamentoServiceImpl.java
@@ -0,0 +1,34 @@
+package com.marcosp.minhasfinancas.service.impl;
+
+import com.marcosp.minhasfinancas.model.entity.Lancamento;
+import com.marcosp.minhasfinancas.model.enums.StatusLancamento;
+import com.marcosp.minhasfinancas.service.LancamentoService;
+
+import java.util.List;
+
+public class LancamentoServiceImpl implements LancamentoService {
+    @Override
+    public Lancamento salvar(Lancamento lancamento) {
+        return null;
+    }
+
+    @Override
+    public Lancamento atualizar(Lancamento lancamento) {
+        return null;
+    }
+
+    @Override
+    public void deletar(Lancamento lancamento) {
+
+    }
+
+    @Override
+    public List<Lancamento> buscar(Lancamento lancamentoFiltro) {
+        return null;
+    }
+
+    @Override
+    public void atualizarStatus(Lancamento lancamento, StatusLancamento status) {
+
+    }
+}
